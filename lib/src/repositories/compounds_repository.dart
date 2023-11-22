3a4c0e7f5393c1d3023401c3c53dc8752d077577
Ianara-cs <ianarasilva333@gmail.com>
2023-11-22 10:27:24 -0300

    adicionando mais compostos organicos

--- lib/src/repositories/compounds_repository.dart
+++ lib/src/repositories/compounds_repository.dart
@@ -4,7 +4,7 @@ Compound legumes = Compound(
   itemName: 'Legumes',
   isUsed: true,
   details:
-      'Legumes são uma excelente adição para uma composteira por várias razões: 1. Ricos em Nutrientes: Legumes são uma fonte rica de nutrientes essenciais, como nitrogênio, fósforo e potássio, que são fundamentais para o crescimento saudável das plantas. Esses nutrientes são valiosos para a produção de composto de alta qualidade. 2. Fornecem Nitrogênio: Os legumes geralmente contêm uma quantidade significativa de nitrogênio, um componente essencial para o processo de decomposição. O nitrogênio ajuda a acelerar a decomposição dos materiais orgânicos na composteira. 3. Textura e Estrutura: A adição de legumes à composteira ajuda a melhorar a textura e a estrutura do composto. Legumes ajudam a arejar o composto, evitando a compactação e melhorando a circulação de ar, o que é vital para a atividade microbiana saudável. 4. Diversidade de Materiais: Uma composteira eficaz requer uma variedade de materiais para alcançar o equilíbrio certo de carbono e nitrogênio. Os legumes, sendo uma fonte de nitrogênio, complementam bem os materiais ricos em carbono, como folhas secas e aparas de madeira. 5. Contribuem para pH Neutro: Muitos legumes têm um efeito neutro ou ligeiramente alcalino no pH, o que é benéfico para o composto. Isso ajuda a equilibrar materiais mais ácidos, como frutas cítricas. 6. Resíduos de Cozinha: Legumes muitas vezes são parte dos resíduos de cozinha, o que os torna uma fonte prontamente disponível para compostagem. Ao usar restos de legumes na composteira, você está reduzindo a quantidade de resíduos enviados para aterros sanitários. 7.Redução de Resíduos: Utilizar legumes na compostagem ajuda a reduzir o desperdício de alimentos e contribui para práticas mais sustentáveis, fechando o ciclo de nutrientes. Lembre-se de equilibrar a adição de legumes com materiais ricos em carbono, como palha, folhas secas ou papel picado, para garantir uma boa relação de carbono/nitrogênio. Isso promove uma decomposição eficiente e evita problemas como odores desagradáveis e excesso de umidade na composteira.',
+      'Legumes na vermicompostagem é uma fonte de nutrientes essenciais para minhocas e plantas. As minhocas são animais invertebrados que desempenham um papel fundamental no processo de vermicompostagem. Elas são responsáveis por decompor os resíduos orgânicos, como folhas, frutas, verduras e legumes, transformando-os em um composto rico em nutrientes. Os legumes são uma ótima fonte de alimento para as minhocas. Eles são ricos em carboidratos, proteínas e gorduras, que são os nutrientes essenciais para as minhocas sobreviverem e se reproduzirem. Além disso, os legumes também são uma boa fonte de fibra, que ajuda a manter o vermicompostador úmido e aerado.',
   imgPath: 'images/compounds/Legumes.jpg',
 );
 
@@ -12,30 +12,85 @@ Compound frutas = Compound(
   itemName: 'Frutas',
   isUsed: true,
   details:
-      'Frutas são uma excelente adição para uma composteira por várias razões: 1. Ricas em Nutrientes: Frutas são ricas em nutrientes essenciais, como vitaminas e minerais. Ao compostar frutas, você está adicionando esses nutrientes valiosos ao seu composto, enriquecendo-o. 2. Fornecem Nitrogênio: Muitas frutas contêm quantidades significativas de nitrogênio, que é um componente essencial para acelerar o processo de decomposição na composteira. 3. Aceleram a Decomposição: A alta quantidade de umidade nas frutas ajuda a acelerar a decomposição dos materiais orgânicos na composteira. Isso é especialmente útil se a composteira estiver um pouco seca, já que as frutas contribuem para a umidade. 4. Resíduos de Cozinha: Semelhante aos legumes, as frutas são frequentemente consumidas na cozinha, gerando resíduos que podem ser facilmente compostados. Isso reduz a quantidade de resíduos enviados para aterros sanitários. 5. Contribuem para o Aroma: As frutas adicionam um aroma agradável à compostagem, ajudando a mascarar odores desagradáveis que podem surgir durante o processo de decomposição. 6. Variedade de Materiais: Assim como legumes, frutas adicionam variedade ao composto. A diversidade de materiais na composteira é importante para obter um equilíbrio adequado de carbono e nitrogênio. 7. Promovem a Biodiversidade Microbiana: As frutas proporcionam uma fonte adicional de carbono e nutrientes, promovendo a diversidade microbiana no composto. Uma comunidade microbiana saudável é crucial para um processo de compostagem eficiente. Ao adicionar frutas à composteira, é importante equilibrar sua contribuição com materiais ricos em carbono, como palha, folhas secas e papel picado, para manter a relação adequada de carbono/nitrogênio e evitar problemas como odores desagradáveis e excesso de umidade.',
+      'Frutas são materiais orgânicos ricos em nutrientes, que podem ser usados na vermicompostagem. Elas são uma boa fonte de carboidratos, proteínas, gorduras, vitaminas e minerais, que são essenciais para a dieta das minhocas. Além disso, frutas ajudam a manter o vermicompostador úmido e aerado. As minhocas são capazes de digerir frutas. Elas quebram as paredes celulares das frutas, liberando os nutrientes internos. Esses nutrientes são então utilizados pelas minhocas para sua alimentação e crescimento. Frutas podem ser adicionadas à vermicompostagem inteiras ou trituradas. Frutas inteiras são mais difíceis de serem digeridos pelas minhocas, mas elas fornecem mais estrutura ao vermicompostador. Frutas trituradas são mais fáceis de serem digeridos pelas minhocas, mas elas podem atrair insetos e roedores. É importante notar que algumas frutas, como bananas, abacates e frutas muito maduras, podem ser muito ricas em açúcares e causar fermentação no vermicompostador. Se você estiver usando essas frutas, é importante adicioná-las em pequenas quantidades e misturadas com outros resíduos orgânicos.',
   imgPath: 'images/compounds/frutas.jpg',
+);
+
+Compound borra = Compound(
+  itemName: 'Borra de Café',
+  isUsed: true,
+  details:
+      'A borra de café é um material orgânico rico em nutrientes, que pode ser usado na vermicompostagem. As minhocas gostam de comer borra de café, pois ela é rica em carbono, um nutriente essencial para a sua dieta. Além disso, a borra de café ajuda a manter o vermicompostador úmido e aerado. No entanto, é importante usar borra de café na vermicompostagem com moderação. Se for usada em excesso, a borra de café pode causar: Acidez excessiva. A borra de café é naturalmente ácida, e se for usada em excesso, pode tornar o vermicompostador muito ácido. Isso pode prejudicar a saúde das minhocas e dificultar o processo de compostagem; Mau cheiro. A borra de café pode apodrecer e causar mau cheiro se for usada em excesso; Atração de insetos e roedores. A borra de café pode atrair insetos e roedores, que podem prejudicar o vermicompostador e espalhar doenças. Portanto, é recomendado usar borra de café na vermicompostagem em quantidades de até 20% do peso total dos resíduos orgânicos que são adicionados ao vermicompostador.',
+  imgPath: 'images/compounds/borra-cafe.jpg',
+);
+
+Compound cascaOvo = Compound(
+  itemName: 'Casca de Ovo',
+  isUsed: true,
+  details:
+      'A casca de ovo é um material orgânico rico em nutrientes, que pode ser usado na vermicompostagem. Ela é composta principalmente de cálcio, que é um nutriente essencial para as plantas. Além disso, a casca de ovo ajuda a manter o vermicompostador úmido e aerado. As minhocas não conseguem digerir a casca de ovo, mas elas podem quebrá-la em pequenos pedaços. Esses pedaços são então decompostos por bactérias e fungos, que liberam o cálcio e outros nutrientes no vermicompostador. A casca de ovo pode ser adicionada à vermicompostagem de duas maneiras: Casca de ovo triturada. A casca de ovo pode ser triturada em um liquidificador ou processador de alimentos. A casca triturada é mais fácil de ser digerida pelas minhocas e ajuda a manter o vermicompostador úmido e aerado; Casca de ovo inteira. A casca de ovo também pode ser adicionada à vermicompostagem inteira. As minhocas irão quebrar a casca de ovo ao longo do tempo, liberando o cálcio e outros nutrientes. Se notar que o vermicompostador está com mau cheiro, remova as cascas de ovo do vermicompostador e reduza a quantidade que você está adicionando.',
+  imgPath: 'images/compounds/casca-ovo.jpg',
+);
+
+Compound sementes = Compound(
+  itemName: 'Grãos e Sementes',
+  isUsed: true,
+  details:
+      'Grãos e sementes são materiais orgânicos ricos em nutrientes, que podem ser usados na vermicompostagem. Eles são uma boa fonte de carboidratos, proteínas e gorduras, que são essenciais para a dieta das minhocas. Além disso, grãos e sementes ajudam a manter o vermicompostador úmido e aerado. As minhocas são capazes de digerir grãos e sementes. Elas quebram as paredes celulares dos grãos e sementes, liberando os nutrientes internos. Esses nutrientes são então utilizados pelas minhocas para sua alimentação e crescimento. Grãos e sementes podem ser adicionados à vermicompostagem inteiros ou triturados. Grãos e sementes inteiros são mais difíceis de serem digeridos pelas minhocas, mas eles fornecem mais estrutura ao vermicompostador. Grãos e sementes triturados são mais fáceis de serem digeridos pelas minhocas, mas eles podem atrair insetos e roedores.',
+  imgPath: 'images/compounds/sementes.jpg',
+);
+
+Compound materiaSeca = Compound(
+  itemName: 'Matéria Seca',
+  isUsed: true,
+  details:
+      'A matéria seca é um componente importante da compostagem. Ela ajuda a manter o composto úmido e aerado, e também fornece nutrientes para os microrganismos que decompõem os resíduos orgânicos. O teor de matéria seca ideal para a compostagem varia de acordo com o tipo de material orgânico sendo compostado e com o clima. Em geral, o teor de matéria seca deve ser de 30-60%. Se o teor de matéria seca for muito baixo, o composto ficará seco e difícil de decompor. Se o teor de matéria seca for muito alto, o composto ficará úmido e pode fermentar, produzindo um cheiro desagradável. Para garantir que o teor de matéria seca esteja correto, é importante misturar materiais orgânicos com diferentes teores de umidade. Por exemplo, se você estiver compostando restos de alimentos, que são ricos em água, pode adicionar serragem ou folhas secas, que são ricas em matéria seca. Aqui estão algumas dicas para adicionar matéria seca à compostagem: Use materiais orgânicos ricos em matéria seca, como serragem, folhas secas, palha ou galhos; Adicione a matéria seca em pequenas porções, misturando-a bem com os outros materiais orgânicos; Não adicione muita matéria seca de uma vez, pois isso pode prejudicar o processo de compostagem. Se você notar que o seu composto está muito seco, pode adicionar água ou umedecer os materiais orgânicos antes de adicioná-los ao compostador. Se você notar que o seu composto está muito úmido, pode adicionar matéria seca ou deixar o composto secar ao ar livre por um tempo. Com a atenção adequada ao teor de matéria seca, você pode criar um composto rico em nutrientes e pronto para ser usado em suas plantas.',
+  imgPath: 'images/compounds/materia-seca.jpg',
 );
 
 Compound carne = Compound(
   itemName: 'Carne',
   isUsed: false,
-  details: 'dddddddd',
+  details:
+      'A carne é um alimento rico em proteínas e gorduras. Essas substâncias são difíceis de serem decompostas pelas minhocas, o que pode causar problemas para elas e para o vermicompostador. Para as minhocas, a carne é um alimento indigesto. Ela pode causar problemas digestivos, como diarreia e vômitos, e até mesmo a morte das minhocas. Além disso há outros motivos como: a carne pode apodrecer e causar mau cheiro, pode atrair insetos e roedores, que podem danificar o vermicompostador e espalhar doenças e as bactérias que causam doenças podem se proliferar na carne em decomposição. Portanto, é importante evitar o uso de carne na vermicompostagem.',
   imgPath: 'images/compounds/carne.jpg',
+);
+
+Compound alimentoProcessados = Compound(
+  itemName: 'Alimentos processados',
+  isUsed: false,
+  details:
+      'Alimentos processados, como frituras, salgadinhos, embutidos, doces, e alimentos industrializados, não devem ser usados na vermicompostagem. Esses alimentos são ricos em gorduras, açúcares, conservantes e outros ingredientes artificiais que são difíceis de serem decompostos pelas minhocas e podem causar: acúmulo de matéria orgânica pois as minhocas não conseguem decompor os alimentos processados com eficiência, o que pode causar o acúmulo de matéria orgânica no vermicompostador; os alimentos processados podem apodrecer e causar mau cheiro; os alimentos processados podem atrair insetos e roedores, que podem danificar o vermicompostador e espalhar doenças. Além disso, os alimentos processados podem ser prejudiciais à saúde das minhocas. As minhocas são herbívoras e não são adaptadas a digerir alimentos de origem animal ou artificial. A ingestão de alimentos processados pode causar problemas digestivos, como diarreia e vômitos, e até mesmo a morte das minhocas. Portanto, é importante evitar o uso de alimentos processados na vermicompostagem.',
+  imgPath: 'images/compounds/processados.jpg',
+);
+
+Compound gordura = Compound(
+  itemName: 'Gordura',
+  isUsed: false,
+  details:
+      'A gordura não deve ser usada na vermicompostagem por vários motivos, incluindo: As minhocas não conseguem decompor a gordura com eficiência. Isso pode causar acúmulo de gordura no vermicompostador, o que pode levar ao mau cheiro, à atração de insetos e roedores e até mesmo à morte das minhocas. A gordura pode se solidificar e endurecer no vermicompostador. Isso pode dificultar a circulação do ar e da água, o que pode prejudicar o processo de decomposição. A gordura pode ser prejudicial à saúde das minhocas. As minhocas são herbívoras e não são adaptadas a digerir gordura. A ingestão de gordura pode causar problemas digestivos, como diarreia e vômitos, e até mesmo a morte das minhocas. Portanto, é importante evitar o uso de gordura na vermicompostagem.',
+  imgPath: 'images/compounds/gordura.jpg',
+);
+
+Compound laticinios = Compound(
+  itemName: 'Laticínios',
+  isUsed: false,
+  details:
+      'Os laticínios, como leite, queijo, iogurte, sorvete e creme de leite, não devem ser usados na vermicompostagem por vários motivos, incluindo: As minhocas não conseguem decompor os laticínios com eficiência. Isso pode causar acúmulo de laticínios no vermicompostador, o que pode levar ao mau cheiro, à atração de insetos e roedores e até mesmo à morte das minhocas; Os laticínios podem se solidificar e endurecer no vermicompostador. Isso pode dificultar a circulação do ar e da água, o que pode prejudicar o processo de decomposição; Os laticínios podem ser prejudicial à saúde das minhocas. As minhocas são herbívoras e não são adaptadas a digerir laticínios. A ingestão de laticínios pode causar problemas digestivos, como diarreia e vômitos, e até mesmo a morte das minhocas. Além disso, os laticínios podem atrair moscas-das-frutas, que podem depositar larvas no vermicompostador. Essas larvas podem se alimentar dos laticínios e das minhocas, o que pode prejudicar o processo de compostagem e causar problemas de saúde. Portanto, é importante evitar o uso de laticínios na vermicompostagem.',
+  imgPath: 'images/compounds/laticinio.jpg',
 );
 
 List<Compound> compoundsUsed = [
   legumes,
   frutas,
-  legumes,
-  frutas,
-  legumes,
-  frutas,
-  legumes,
-  frutas,
-  legumes,
-  legumes
+  borra,
+  cascaOvo,
+  sementes,
+  materiaSeca,
 ];
 List<Compound> compoundsNoUsed = [
   carne,
-  carne,
+  alimentoProcessados,
+  gordura,
+  laticinios,
 ];
